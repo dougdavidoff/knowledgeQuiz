ed523930bc6d906f0a6f1ab0bfdf41f4c39d5658
Doug Davidoff <douglass.davidoff@gmail.com>
2020-09-02 15:44:16 -0400

    More images and development

--- script.js
+++ script.js
@@ -1,6 +1,70 @@
 var body = document.body;
 
-var count = 61;
+var count = 126;
+
+var cardContent = {
+    Q1: {
+        image: "./assets/images/globe.jpg",
+        alt: "globe",
+        hed: "Question One",
+        ask: "Bridgeport, Conn., is located near the 41st parallel of north longitude. Which of these is not at 41 degrees north?",
+        responses: {
+            r1: ["1. Island of Sardinia, Italy", false],
+            r2: ["2. Istanbul, Turkey", false],
+            r3: ["3. Beijing, China", false],
+            r4: ["4. Chicago, Illinois", true],
+        }
+        },
+    Q2: {
+        image: "./assets/images/usaMap.png",
+        alt: "USA Map",
+        hed: "Question Two",
+        ask: "The United States has just one 'quadripoint,' where four states meet at a single point. One state below is not at the Four Corners. Which one?",
+        responses: {
+            r1: ["1. Nevada", true],
+            r2: ["2. Arizona", false],
+            r3: ["3. Utah", false],
+            r4: ["4. New Mexico", true],
+        }
+        },
+    Q3: {
+        image: "./assets/images/NYmap.png",
+        alt: "Map of N.Y. State",
+        hed: "Question Three",
+        ask: "New York State borders six U.S. states and two Canadian provinces. These include:",
+        responses: {
+            r1: ["1. Ohio", false],
+            r2: ["2. Delaware", false],
+            r3: ["3. Rhode Island", true],
+            r4: ["4. New Hampshire", false],
+            }
+        },
+    Q4: {
+        image: "./assets/images/connMap.jpg",
+        alt: "Map of Connecticut",
+        hed: "Question Four",
+        ask: "What in heck is the 'Connecticut Oblong?'",
+        responses: {
+            r1: ["1. Official name of the panhandle", false],
+            r2: ["2. Land in New York once part of Connecticut", true],
+            r3: ["3, The notch on the state's northern border", false],
+            r4: ["4. Offcial name of New Haven Green", false],
+            }
+        },
+    Q5: {
+        image: "./assets/images/compassRose.png",
+        alt: "Compass Rose",
+        hed: "Question Five",
+        ask: "What distinguishes Stamford, Connecticut?",
+        responses: {
+            r1: ["1. Most populous city in the state", false],
+            r2: ["2. 3rd-largest financial center in Metro New York", false],
+            r3: ["3. Whether you travel north, south, east, or west, you'll end up in New York State", true],
+            r4: ["4. More land north of Merritt Parkway than south of the Merritt", false],
+            }
+        },    
+    };
+
 
 document.getElementById("beginButton").addEventListener("click", function(){
     removeJumbotron()
@@ -9,26 +73,6 @@ document.getElementById("beginButton").addEventListener("click", function(){
     
     document.getElementById("timer-card").style.display = "block";
     document.getElementById("question-card").style.display = "block";
-    
-    var cardIcon = document.getElementById("card-image");
-    cardIcon.setAttribute("src", "./assets/images/globe.jpg");
-    cardIcon.setAttribute("alt", "globe");
-
-    var cardHed = document.getElementById("card-headline");
-    cardHed.innerHTML = "Question 1";
-
-    var cardQuest = document.getElementById("card-question");
-    cardQuest.innerHTML = "The question is on the motion.";
-
-    var cardAnsA = document.getElementById("card-answer-a");
-    cardAnsA.innerHTML = "Aye";
-
-    var cardAnsB = document.getElementById("card-answer-b");
-    cardAnsB.innerHTML = "Nay";
-
-    var cardAnsC = document.getElementById("card-answer-c");
-    cardAnsC.innerHTML = "Abstain";
-
   });
 
 
@@ -48,41 +92,145 @@ function countdown() {
     }
     document.getElementById("timer").innerHTML = count + " seconds";
 
-    if(count < 55 && count > 45) {
-        var cardIcon = document.getElementById("card-image");
-        cardIcon.setAttribute("src", "./assets/images/usaMap.png");
-        cardIcon.setAttribute("alt", "USA map");
+    
 
-        var cardHed = document.getElementById("card-headline");
-        cardHed.innerHTML = "Question 2";
+
+
+
+    var cardIcon = document.getElementById("card-image");
+    cardIcon.setAttribute("src", cardContent.Q1.image);
+    cardIcon.setAttribute("alt", cardContent.Q1.alt);
+
+    var cardHed = document.getElementById("card-headline");
+    cardHed.innerHTML = cardContent.Q1.hed;
+
+    var cardQuest = document.getElementById("card-question");
+    cardQuest.innerHTML = cardContent.Q1.ask;
+
+    var cardAnsA = document.getElementById("card-answer-a");
+    cardAnsA.innerHTML = cardContent.Q1.responses.r1[0];
     
-        var cardQuest = document.getElementById("card-question");
-        cardQuest.innerHTML = "To be or not to be";
-        
-        document.getElementById("card-answer-a").innerHTML = "It's a question";
-    
-        document.getElementById("card-answer-b").innerHTML = "A noble mind";
-        
-        document.getElementById("card-answer-c").innerHTML = "Slings and arrows";
+    var cardAnsB = document.getElementById("card-answer-b");
+    cardAnsB.innerHTML = cardContent.Q1.responses.r2[0];
+
+    var cardAnsC = document.getElementById("card-answer-c");
+    cardAnsC.innerHTML = cardContent.Q1.responses.r3[0];
+
+    var cardAnsD = document.getElementById("card-answer-d");
+    cardAnsD.innerHTML = cardContent.Q1.responses.r4[0];
+
+
+
+
+
+    if(count < 101 && count >= 76) {
+
+    var cardIcon = document.getElementById("card-image");
+    cardIcon.setAttribute("src", cardContent.Q2.image);
+    cardIcon.setAttribute("alt", cardContent.Q2.alt);
+
+    var cardHed = document.getElementById("card-headline");
+    cardHed.innerHTML = cardContent.Q2.hed;
+
+    var cardQuest = document.getElementById("card-question");
+    cardQuest.innerHTML = cardContent.Q2.ask;
+
+    var cardAnsA = document.getElementById("card-answer-a");
+    cardAnsA.innerHTML = cardContent.Q2.responses.r1[0];
+
+    var cardAnsB = document.getElementById("card-answer-b");
+    cardAnsB.innerHTML = cardContent.Q2.responses.r2[0];
+
+    var cardAnsC = document.getElementById("card-answer-c");
+    cardAnsC.innerHTML = cardContent.Q2.responses.r3[0];
+
+    var cardAnsD = document.getElementById("card-answer-d");
+    cardAnsD.innerHTML = cardContent.Q2.responses.r4[0];
+
     }
 
-    if(count <= 45) {
-        var cardIcon = document.getElementById("card-image");
-        cardIcon.setAttribute("src", "./assets/images/NYmap.png");
-        cardIcon.setAttribute("alt", "New York State map");
 
+    if(count < 76 && count >= 51) {
+
+        var cardIcon = document.getElementById("card-image");
+        cardIcon.setAttribute("src", cardContent.Q3.image);
+        cardIcon.setAttribute("alt", cardContent.Q3.alt);
+    
         var cardHed = document.getElementById("card-headline");
-        cardHed.innerHTML = "Question 3";
+        cardHed.innerHTML = cardContent.Q3.hed;
     
         var cardQuest = document.getElementById("card-question");
-        cardQuest.innerHTML = "This state borders New York";
+        cardQuest.innerHTML = cardContent.Q3.ask;
     
-        document.getElementById("card-answer-a").innerHTML = "Ohio";
+        var cardAnsA = document.getElementById("card-answer-a");
+        cardAnsA.innerHTML = cardContent.Q3.responses.r1[0];
     
-        document.getElementById("card-answer-b").innerHTML = "Delaware";
+        var cardAnsB = document.getElementById("card-answer-b");
+        cardAnsB.innerHTML = cardContent.Q3.responses.r2[0];
+    
+        var cardAnsC = document.getElementById("card-answer-c");
+        cardAnsC.innerHTML = cardContent.Q3.responses.r3[0];
+    
+        var cardAnsD = document.getElementById("card-answer-d");
+        cardAnsD.innerHTML = cardContent.Q3.responses.r4[0];
 
-        document.getElementById("card-answer-c").innerHTML = "Rhode Island";
     }
+
+
+        if(count < 51 && count >= 26) {
+
+            var cardIcon = document.getElementById("card-image");
+            cardIcon.setAttribute("src", cardContent.Q4.image);
+            cardIcon.setAttribute("alt", cardContent.Q4.alt);
+        
+            var cardHed = document.getElementById("card-headline");
+            cardHed.innerHTML = cardContent.Q4.hed;
+        
+            var cardQuest = document.getElementById("card-question");
+            cardQuest.innerHTML = cardContent.Q4.ask;
+        
+            var cardAnsA = document.getElementById("card-answer-a");
+            cardAnsA.innerHTML = cardContent.Q4.responses.r1[0];
+        
+            var cardAnsB = document.getElementById("card-answer-b");
+            cardAnsB.innerHTML = cardContent.Q4.responses.r2[0];
+        
+            var cardAnsC = document.getElementById("card-answer-c");
+            cardAnsC.innerHTML = cardContent.Q4.responses.r3[0];
+        
+            var cardAnsD = document.getElementById("card-answer-d");
+            cardAnsD.innerHTML = cardContent.Q4.responses.r4[0];
+
+        }
+
+
+            if(count < 26 && count >= 0) {
+
+                var cardIcon = document.getElementById("card-image");
+                cardIcon.setAttribute("src", cardContent.Q5.image);
+                cardIcon.setAttribute("alt", cardContent.Q5.alt);
+            
+                var cardHed = document.getElementById("card-headline");
+                cardHed.innerHTML = cardContent.Q5.hed;
+            
+                var cardQuest = document.getElementById("card-question");
+                cardQuest.innerHTML = cardContent.Q5.ask;
+            
+                var cardAnsA = document.getElementById("card-answer-a");
+                cardAnsA.innerHTML = cardContent.Q5.responses.r1[0];
+            
+                var cardAnsB = document.getElementById("card-answer-b");
+                cardAnsB.innerHTML = cardContent.Q5.responses.r2[0];
+            
+                var cardAnsC = document.getElementById("card-answer-c");
+                cardAnsC.innerHTML = cardContent.Q5.responses.r3[0];
+            
+                var cardAnsD = document.getElementById("card-answer-d");
+                cardAnsD.innerHTML = cardContent.Q5.responses.r4[0];
+
+        }
+
+
 }
 
 
